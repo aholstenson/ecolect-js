aa6bb54167aa3ae63595f8068adb64766c803623
Andreas Holstenson <a@holstenson.se>
2017-01-29 18:41:12 +0100

    Correctly transfer times to date

--- language/dates.js
+++ language/dates.js
@@ -36,7 +36,10 @@ function toDate(date, now) {
 	return new Date(
 		typeof date.year !== 'undefined' ? date.year : now.getFullYear(),
 		typeof date.month !== 'undefined' ? date.month : now.getMonth(),
-		typeof date.day !== 'undefined' ? date.day : now.getDate()
+		typeof date.day !== 'undefined' ? date.day : now.getDate(),
+		typeof date.hour !== 'undefined' ? date.hour : 0,
+		typeof date.minute !== 'undefined' ? date.minute : 0,
+		typeof date.second !== 'undefined' ? date.second : 0
 	);
 }
 
