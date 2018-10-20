580de8d8580436720ac7bccb530ec0c38be299be
Andreas Holstenson <a@holstenson.se>
2018-10-20 15:11:44 +0200

    chore: Show options used in tests

--- test/language/helpers.js
+++ test/language/helpers.js
@@ -1,8 +1,27 @@
 import { expect } from 'chai';
+import { format } from 'date-fns';
+
+function formatDate(d) {
+	if(! d) return 'current time';
+
+	return format(d, 'yyyy-MM-dd HH:mm');
+}
+
+function formatOptions(opts) {
+	let t = formatDate(opts.now);
+
+	for(const key of Object.keys(opts)) {
+		if(key === 'now') continue;
+
+		t += ', ' + key + '=' + opts[key];
+	}
+
+	return t;
+}
 
 function createTester(f, matcher) {
 	return function(expr, opts, v) {
-		f(expr + ' [' + (opts.now ? opts.now.toString() : 'current time') + ']', () => {
+		f(expr + ' [' + formatOptions(opts) + ']', () => {
 			return matcher(expr, opts)
 				.then(r => {
 					expect(r).to.deep.equal(v);
