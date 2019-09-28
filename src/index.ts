a91f35bc2f5a1abda972355621c20c626e6effaf
Andreas Holstenson <a@holstenson.se>
2019-09-28 19:33:47 +0200

    fix: Typescript entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+import { Language } from './language/language';
+
 import { IntentsBuilder } from './intents';
 import { ActionsBuilder } from './actions';
 
 export * from './intents';
 export * from './actions';
 
-export function intentsBuilder(lang) {
+export function intentsBuilder(lang: Language) {
 	return new IntentsBuilder(lang);
 }
 
-export function actionsBuilder(lang) {
+export function actionsBuilder(lang: Language) {
 	return new ActionsBuilder(lang);
 }
